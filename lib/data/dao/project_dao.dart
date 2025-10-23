e0d7957894c2b6336e3e0c69b246de4913a02e59
Yaroslav Rud <125079644+RudiYaroslav@users.noreply.github.com>
2025-10-23 22:02:31 +0300

    Update project_dao.dart

--- lib/data/dao/project_dao.dart
+++ lib/data/dao/project_dao.dart
@@ -4,6 +4,7 @@ part of '../database/project_database.dart';
 class ProjectsDao extends DatabaseAccessor<ProjectDatabase> with _$ProjectsDaoMixin {
   ProjectsDao(super.database);
 
+  // можно вместо ProjectsTableCompanion.insert вставить ProjectsTableData
   Future<void> putProject() => into(projectsTable).insert(ProjectsTableCompanion.insert(name: 'value',  userId: 1));
 
   Future<List<ProjectsTableData>> getAll() => select(projectsTable).get();
